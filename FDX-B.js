974eb9e07208cfaf4c2b47e4b0ba6a458f58be4c
victorcanoz <victorcanoz@ikalogic.com>
2019-04-10 09:26:00 +0200

    Update FDX-B
    Added CRC frame checking

--- FDX-B.js
+++ FDX-B.js
@@ -14,6 +14,24 @@ V0.0:  Initial release.
 </RELEASE_NOTES>
 */
 
+function crc16(bytes) {
+  crc = 0;
+  polynomial = 0x1021;
+  for (j=0;j<bytes.length;j++)
+  {
+      b = bytes[j];
+      for (i = 0; i < 8; i++)
+      {
+          bit = ((b >> (7 - i) & 1) == 1);
+          c15 = ((crc >> 15 & 1) == 1);
+          crc <<= 1;
+          if (c15 ^ bit) crc ^= polynomial;
+      }
+  }
+  crc &= 0xffff;
+  return crc;
+}
+
 //Decoder GUI
 function on_draw_gui_decoder()
 {
@@ -31,7 +49,9 @@ function on_draw_gui_decoder()
     ScanaStudio.gui_end_container();
   ScanaStudio.gui_end_selectable_containers_group();
   //Add hidden elements for the FDX-B decoder
-  ScanaStudio.gui_add_hidden_field("th","178.83755588673621460506706408346e-6"); // (1/(134.2kHz/32))*3/4 => refer to biphase encoding
+  var carrier_frequency = 134.2e3;
+  var pulse_width = (1/(carrier_frequency/32))*3/4; //=> refer to biphase encoding
+  ScanaStudio.gui_add_hidden_field("th",pulse_width.toString(10));
 }
 
 var states = {"waiting_header":1, "getting_data":2, "checking":3, "parsing":4, "optional_data":5};
@@ -96,6 +116,7 @@ function on_decode_signals(resume)
         }
         header_cnt++;
         break;
+
       case "getting_data":
         data[data_cnt] = biphase_items[i].content;
         if (data_cnt >= data_len-1)
@@ -110,9 +131,9 @@ function on_decode_signals(resume)
         }
         data_cnt++;
         break;
+
       case "checking":
         var ok = true;
-
         //Checking control bits and remove them
         for (j = 0; j < data_len; j++)
         {
@@ -136,19 +157,33 @@ function on_decode_signals(resume)
         for (j = 0; j < data_len_without_control_bits; j+=8)
         {
           binary_str = "";
-          for (k = 0; k < 8; k++)
+          for (k = 0; k <8; k++) //Data received is LSB first, however, we have to use it as it is for the CRC calculation
           {
             binary_str += data_without_control_bits[j+k];
           }
           bytes[j/8] = parseInt(binary_str, 2);
-          ScanaStudio.console_info_msg("bytes[" + (j/8) + "] = " + binary_str + " = " + bytes[j/8]);
+          //ScanaStudio.console_info_msg("bytes[" + (j/8) + "] = " + binary_str + " = " + bytes[j/8]);
         }
 
+        //Check checksum
+        var data_to_check = bytes.slice(0,8);
+        var computed_crc = crc16(data_to_check);
+        var read_crc = (bytes[8]<<8)|(bytes[9]);
 
-        state_machine = "waiting_header";
+        if (computed_crc == read_crc)
+        {
+          ScanaStudio.console_info_msg("Valid frame (CRC = 0x"+read_crc.toString(16)+")");
+        }
+        else {
+          ScanaStudio.console_warning_msg("Corrupted message: wrong CRC");
+          ScanaStudio.console_info_msg("Computed CRC16-CCITT of incoming data is 0x" + computed_crc.toString(16));
+          ScanaStudio.console_info_msg("CRC read from the data is 0x" + read_crc.toString(16));
+        }
+        state_machine = "parsing";
         break;
+
       case "parsing":
-        //ScanaStudio.console_info_msg("Parsing data");
+
         break;
     }
   }
