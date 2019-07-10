cb82f3939fd1ecae9b4009e306d209be746298dc
IKALOGIC <contact@ikalogic.com>
2019-07-10 19:17:23 +0200

    remove bad character

--- gray_code.js
+++ gray_code.js
@@ -11,7 +11,7 @@ gray code support for ScanaStudio.
 <LICENSE>	This code is distributed under the terms of the GNU General Public License GPLv3 </LICENSE>
 <RELEASE_NOTES>
 	V1.1: Added CSV plotting option
-	V1.0: Initial release & create builder demo
+	V1.0: Initial release and create builder demo
 	V1.1: Decode signial
 </RELEASE_NOTES>
 */
